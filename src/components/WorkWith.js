7ff4fba3d91ef99326ef85ba3e3dd1d2f986c8a1
AgusSoria <leandroagussoria472@gmail.com>
2024-04-08 21:10:24 -0300

    ultimos cambios

---
+++ src/components/WorkWith.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../App.css';
+
+const WorkWith = () => {
+  const h1Style = {
+    fontSize: '30px',
+  }
+  return (
+    <div>
+      <h1 style={h1Style}>Las distintas modalidades de sesion</h1>
+    </div>
+  );
+};
+
+export default WorkWith;
